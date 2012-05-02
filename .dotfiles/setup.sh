df97d8b55cde5f14413f50311a83037c55e7e998
Brad Bowman <bsb@strategicdata.com.au>
2012-05-02 11:36:40 +1000

    Break a circularity in setup.sh

--- .dotfiles/setup.sh
+++ .dotfiles/setup.sh
@@ -4,11 +4,11 @@ git clone --bare -n git://github.com/$(git config github.user || echo bowman)/.d
 git config -f .dotfiles/repo.git/config core.bare false
 git config -f .dotfiles/repo.git/config core.logallrefupdates true
 git config -f .dotfiles/repo.git/config core.worktree $HOME
-#export GIT_DIR=~/.dotfiles/repo.git ;  export GIT_WORK_TREE=~
-source <( git show :.dotfiles/on )
-git read-tree -v HEAD 
+export GIT_DIR=~/.dotfiles/repo.git ;  export GIT_WORK_TREE=~
+git read-tree -v HEAD
 git checkout-index -a # all files without overwriting
+source ~/.dotfiles/on
 git status
 ln -sf $HOME/.dotfiles/gitignore-dots $HOME/.dotfiles/repo.git/info/exclude
 git status
-# . .dotfiles/on
+echo ". .dotfiles/on # to enter dotfiles edit mode"
