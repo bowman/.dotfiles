6ff4259aa4598ec891dafb44be9f53f9a080b338
Brad Bowman <bsb@strategicdata.com.au>
2012-05-07 14:18:46 +1000

    PS1 with host over ssh

--- .bash.d/available/vars.ps1
+++ .bash.d/available/vars.ps1
@@ -3,17 +3,26 @@
 
 type -t has > /dev/null || . .bash.d/available/func.has
 
+# export PROMPT_DIRTRIM=2
 
 if [ `id -u` = 0 ]; then
     # root gets a red prompt
     export PS1='\[\033[01;31m\]\u@\h\[\033[00m\]:\w # '
 
-elif has __git_ps1 ; then
-    # http://zerokspot.com/weblog/2008/12/04/git-branches-and-ps1/
-    export PS1="[\$(date +%H:%M) \W"'$(__git_ps1 " (%s)")]$ '
-
 else
-    export PS1="[\$(date +%H:%M) \W]$ "
+
+    if [ "z$SSH_CONNECTION" != "z" ] ; then
+        PS1='[\u@\h:\W'
+    else
+        PS1='[$(date +%H:%M) \W'
+    fi
+
+    if has __git_ps1 ; then
+        # http://zerokspot.com/weblog/2008/12/04/git-branches-and-ps1/
+        PS1=$PS1'$(__git_ps1 " (%s)")'
+    fi
+
+    export PS1=$PS1']$ '
 fi
 
 # vim: syntax=sh :
